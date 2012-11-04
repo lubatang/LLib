f9b37054836a5ef6d50252fd6ec4442a39ecfe1b
Luba Tang <lubatang@gmail.com>
2012-11-05 01:28:04 +0800

    Fix a bug - my stupid fault to add Triangle.cpp.

---
+++ lib/Triangle.cpp
@@ -0,0 +1,13 @@
+//===- Triangle.cpp -------------------------------------------------------===//
+//
+// Copyright (C), 2012-
+// Luba Tang <lubatang@gmail.com>
+//===----------------------------------------------------------------------===//
+#include <Triangle/Triangle.h>
+#include <Triangle/Vertex.h>
+
+using namespace luba;
+
+Triangle::Triangle(const Vertex& pV1, const Vertex& pV2, const Vertex& pV3)
+{
+}
