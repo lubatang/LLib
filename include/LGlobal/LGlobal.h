6219a10a36ac09538ac41263a040013ce8429424
Luba Tang <lubatang@gmail.com>
2012-12-05 02:56:03 +0800

    Refine format.

--- include/LGlobal/LGlobal.h
+++ include/LGlobal/LGlobal.h
@@ -1,14 +1,10 @@
-
 #ifndef _LGLOBAL_H_
 #define _LGLOBAL_H_
 
-namespace LLib
-{
-  namespace Math
-  {
-    static const double    PI              =  3.1415926535897932384626433832795;
-  };
-
-}
+namespace LLib {
+namespace Math {
+  static const double    PI              =  3.1415926535897932384626433832795;
+} // namespace of Math
+} // namespace of LLib
 
 #endif
