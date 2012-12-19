8fecd75160027ed278cd80e42c7c330e29ad8e49
Luba Tang <lubatang@gmail.com>
2012-12-19 14:15:04 +0800

    Add setX(), setY(), setZ().

--- include/Triangle/Vertex.h
+++ include/Triangle/Vertex.h
@@ -18,15 +18,20 @@ public:
   const Coord& coord() const { return m_Coord; }
   Coord&       coord()       { return m_Coord; }
 
+  Vertex& setCoord(double pX, double pY, double pZ);
+  void setX(double pX) { m_Coord[0] = pX; }
+  void setY(double pY) { m_Coord[1] = pY; }
+  void setZ(double pZ) { m_Coord[2] = pZ; }
+
+
   const Color& color() const { return m_Color; }
   Color&       color()       { return m_Color; }
 
+  // -----  accessors  ----- //
   double  x() const { return m_Coord[0]; }
   double& x()       { return m_Coord[0]; }
-
   double  y() const { return m_Coord[1]; }
   double& y()       { return m_Coord[1]; }
-
   double  z() const { return m_Coord[2]; }
   double& z()       { return m_Coord[2]; }
 
