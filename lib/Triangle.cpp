e2309d489c0fa71f5857085c17dadff400f54e1e
Luba Tang <lubatang@gmail.com>
2013-01-02 03:54:46 +0800

    Fix a bug - fix the order of triangle's verteices.

--- lib/Triangle.cpp
+++ lib/Triangle.cpp
@@ -12,12 +12,28 @@ using namespace luba;
 
 Triangle::Triangle(const Vertex& pV1, const Vertex& pV2, const Vertex& pV3)
   : m_pV1(&pV1), m_pV2(&pV2), m_pV3(&pV3) {
-  // sort by Y
   Coord v[3];
   v[0] = pV1.coord();
   v[1] = pV2.coord();
   v[2] = pV3.coord();
 
+  // sort X from larger to smaller 
+  if (v[0][0] < v[1][0]) {
+    std::swap(v[0], v[1]);
+    std::swap(m_pV1, m_pV2);
+  }
+
+  if (v[0][0] < v[2][0]) {
+    std::swap(v[0], v[2]);
+    std::swap(m_pV1, m_pV3);
+  }
+
+  if (v[1][0] < v[2][0]) {
+    std::swap(v[1], v[2]);
+    std::swap(m_pV2, m_pV3);
+  }
+
+  // sort Y from larger to smaller
   if (v[0][1] < v[1][1]) {
     std::swap(v[0], v[1]);
     std::swap(m_pV1, m_pV2);
