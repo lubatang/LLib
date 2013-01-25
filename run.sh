409afc5c1abce44a87a19bdbbb29754de3357735
Luba Tang <lubatang@gmail.com>
2013-01-25 08:17:23 +0800

    Add bump map API.

--- run.sh
+++ run.sh
@@ -13,6 +13,20 @@ DIRECTORY=$1
 
 files=`find ${DIRECTORY} | grep --color=never ".obj$"`
 
+echo "Bump map path? (skip this step by typing \`enter')";
+read BUMP_MAP;
+if [ ! -z ${BUMP_MAP} ]; then
+    while [ ! -f ${BUMP_MAP} ]; do
+      echo "Can not find file \`${BUMP_MAP}'. Please to key-in the path again.";
+      ls
+      read BUMP_MAP;
+    done
+fi
+
 for file in ${files}; do
-  ${VIEWER} -f ${file}
+  if [ -z ${BUMP_MAP} ]; then
+    ${VIEWER} -f ${file}
+  else
+    ${VIEWER} -f ${file} -b ${BUMP_MAP}
+  fi
 done
